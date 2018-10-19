493680392c6a9b7e5d857b4a3015e5fd020b448b
ivanbkovacevic <ivanbkovacevic@gmail.com>
2018-10-19 23:02:56 +0200

    na server.js

--- server.js
+++ server.js
@@ -59,8 +59,8 @@
 
  if(process.env.NODE_ENV==='production'){
      //Set static folder
-     app.use(express.static(client/build));
-     app.get('*',(req,res)=>{
+     app.use(express.static('client/build'));
+     app.get('*',(req, res)=>{
          res.sendFile(path.resolve(__dirname,'client','build','index.html'));
      });
 
